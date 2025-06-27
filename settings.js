4dcf72f359fe77e369f0cf7e72ac09169f67f75c
vikkont79 <vvikkont79@yandex.ru>
2025-06-27 13:13:59 +0300

    Создание проекта без сборки

---
+++ settings.js
@@ -0,0 +1,16 @@
+// Файл для настроек приложения
+const TIMEOUT = 900;
+
+const AppRoute = {
+  Start: 'start',
+  Game: 'game',
+  Results: 'results',
+};
+
+const GAME_TYPES = [
+  { type: 'cats', icon: 'cats', label: 'Котики' },
+  { type: 'flowers', icon: 'flowers', label: 'Цветочки' },
+  { type: 'cars', icon: 'cars', label: 'Машины' }
+];
+
+const RANDOMIZE = true;
