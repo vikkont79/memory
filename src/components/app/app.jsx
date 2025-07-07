50569c566e7880fb1cdca5a34855b67072c37d62
vikkont79 <vvikkont79@yandex.ru>
2025-07-07 17:29:15 +0300

    Заменил условный рендеринг на декларативную маршрутизацию через React Router

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,48 +1,54 @@
 import React from 'react';
 import { useState } from 'react';
 import { AppRoute } from '../../settings';
-
 import { GamePage } from '../game-page/game-page.jsx';
 import { ResultsPage } from '../results-page/results-page.jsx';
-import { StartPage} from '../start-page/start-page.jsx';
+import { StartPage } from '../start-page/start-page.jsx';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 
 function App({ getImages, results = [] }) {
-  const [page, setPage] = useState(AppRoute.Start);
+  const navigate = useNavigate();
   const [result, setResult] = useState(0);
   const [images, setImages] = useState([]);
   const [cardsType, setCardsType] = useState(null);
 
   const showResults = (stepsCount) => {
     setResult(stepsCount);
-    setPage(AppRoute.Results);
+    navigate(AppRoute.Results);
   };
 
   const handleReset = () => {
-    setPage(AppRoute.Start);
+    navigate(AppRoute.Start);
   };
 
   const handleStart = (type) => {
     setImages(getImages(type));
     setCardsType(type);
-    setPage(AppRoute.Game);
+    navigate(AppRoute.Game);
   };
 
-  const getPage = (route) => {
-    switch (route) {
-      case AppRoute.Start:
-        return <StartPage onStart={handleStart} />
-      case AppRoute.Game:
-        return <GamePage images={images} type={cardsType} onShowResults={showResults} />;
-      case AppRoute.Results:
-        return (
-          <ResultsPage results={results} playerResult={result} onResetGame={handleReset}
-          />
-        );
-      default:
-        return null;
-    }
-  };
-  return getPage(page);
+  return (
+    <Routes>
+      <Route index element={<StartPage onStart={handleStart} />} />
+      <Route
+        path={AppRoute.Game}
+        element={
+          <GamePage
+            images={images}
+            type={cardsType}
+            onShowResults={showResults} />}
+      />
+      <Route
+        path={AppRoute.Results}
+        element={
+          <ResultsPage
+            results={results}
+            playerResult={result}
+            onResetGame={handleReset}
+          />}
+      />
+    </Routes>
+  )
 }
 
 export { App };
--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import { createRoot } from 'react-dom/client'
 import { results, getImages } from './data';
 import { App } from './components/app/app.jsx';
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App getImages={getImages} results={results} />
+    <BrowserRouter>
+      <App getImages={getImages} results={results} />
+    </BrowserRouter>
   </React.StrictMode>
 )
